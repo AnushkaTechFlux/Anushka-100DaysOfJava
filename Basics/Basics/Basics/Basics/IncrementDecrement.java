65a498f929af31c9baae3fa6f9a10333f201f831
anushhka sharma <177105799+AnushkaTechFlux@users.noreply.github.com>
2025-10-25 09:00:39 +0530

    IncrementDecrement.java

---
+++ Basics/Basics/Basics/Basics/IncrementDecrement.java
@@ -0,0 +1,20 @@
+package Basics;
+
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int x = 10;           // initialize x with 10
+        System.out.println(x); // prints 10
+
+        x += 7;               // x = x + 7
+        System.out.println(x); // prints 17
+
+        x -= 2;               // x = x - 2
+        System.out.println(x); // prints 15
+
+        x *= 5;               // x = x * 5
+        System.out.println(x); // prints 75
+
+        x /= 2;               // x = x / 2
+        System.out.println(x); // prints 37
+    }
+}
