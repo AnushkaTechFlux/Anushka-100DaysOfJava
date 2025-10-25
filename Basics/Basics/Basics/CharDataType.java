b9469b561666c50536a7e84bc59808d121682ad0
anushhka sharma <177105799+AnushkaTechFlux@users.noreply.github.com>
2025-10-25 08:56:00 +0530

    CharDataType.java

---
+++ Basics/Basics/Basics/CharDataType.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class CharDataType {
+    public static void main(String[] args) {
+        // Examples of characters in Java
+        // Lowercase letters: a b c d e ... z
+        // Uppercase letters: A B C D ... Z
+        // Digits: 0 1 2 3 4 5 6 7 8 9
+        // Special symbols: ~ ! @ # $ % ^ & * ( ) _ - + = { } [ ] | \ : ; " ' < , > . / ?
+        
+        char ch = '5';  // storing a single character
+        System.out.println(ch);  // prints the character
+    }
+}
