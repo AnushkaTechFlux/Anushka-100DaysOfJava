e9b57d51df32ae7c5842cf5a66b345e3dfc5be3d
anushhka sharma <177105799+AnushkaTechFlux@users.noreply.github.com>
2025-10-25 08:58:10 +0530

    Comments.java

---
+++ Basics/Basics/Basics/Basics/Comments.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class Comments {
+    public static void main(String[] args) {
+        // This is a single-line comment
+
+        /* This is a multi-line comment
+           You can write a paragraph here
+           spanning multiple lines
+        */
+
+        // Tip: In most IDEs, you can press Ctrl/Cmd + / to comment/uncomment lines
+    }
+}
